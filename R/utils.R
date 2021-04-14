27f0f06b2f7c1fef364cd6bc09b97484987d7f48
Duncan Wilson <meddwilb@leeds.ac.uk>
2021-04-14 19:27:28 +0100

    add init_DoE

--- R/utils.R
+++ R/utils.R
@@ -13,3 +13,31 @@
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
+
+#' Set up initial space-filling design
+#'
+#' @param size integer
+#' @param design_space data.frame
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' design_space <- data.frame(name = c("n", "k"),
+#' low = c(100, 10),
+#' up = c(500, 100),
+#' int = c(T, T))
+#'
+#' init_DoE(10, design_space)
+init_DoE <- function(size, design_space)
+{
+  dim <- nrow(design_space)
+  DoE <- data.frame(sobol(size, dim))
+  names(DoE) <- design_space$name
+  for(i in 1:dim){
+    DoE[,i] <-  DoE[,i]*(design_space$up[i]-design_space$low[i]) + design_space$low[i]
+  }
+  DoE[, design_space$int] <- round(DoE[, design_space$int])
+
+  DoE
+}
