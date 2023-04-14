b9dbbe3e5a9e686f80738e80ee7127823d30c0cc
Duncan Wilson <meddwilb@leeds.ac.uk>
2023-04-14 14:26:34 +0100

    Added sensitivity analysis function

---
+++ R/sensitivity.R
@@ -0,0 +1,39 @@
+#' Run a one-dimensional sensitivity analysis
+#'
+#' @param sa_hypothesis Data frame defining the SA variable and its bounds
+#' @param design Design to be tested
+#' @param problem BOSSS problem
+#' @param solution BOSSS solution
+#' @param num_eval Number of points to evaluate
+#' @param N Number of MC samples to use in each evaluation
+#'
+#' @return A matrix of estimated means and their variances for each simulation
+#' output over the SA variable range.
+#' @export
+#'
+sensitivity <- function(sa_hypothesis, design, problem, solution, num_eval = 20, N = 100) {
+
+    # Create the grid to evaluate, varying only the SA parameter
+    to_eval <- matrix(rep(problem$hypotheses[,sa_hypothesis$hyp], each = num_eval), ncol = nrow(problem$hypotheses))
+    to_eval[, sa_hypothesis$sa_param] <- seq(sa_hypothesis$lower, sa_hypothesis$upper, length.out = num_eval)
+
+    # Run simulations for the given design at all these hypotheses
+    results <- NULL
+    for(j in 1:num_eval) {
+      sims <- replicate(N, problem$simulation(design, as.data.frame(to_eval)[j,]))
+      r <- NULL
+      n <- NULL
+      for(k in 1:nrow(sims)){
+        # Results are the mean and variance of each of the simulation outputs
+        r <- c(r, c(mean(sims[k,]), stats::var(sims[k,])/N))
+        n <- c(n, c(paste0(rownames(sims)[k], "_m"), paste0(rownames(sims)[k], "_v")))
+      }
+      results <- rbind(results, r)
+    }
+    colnames(results) <- n
+
+    results <- cbind(to_eval[, sa_hypothesis$sa_param], results)
+    colnames(results)[1] <- rownames(problem$hypotheses)[sa_hypothesis$sa_param]
+
+    return(results)
+}
