fbcd37eb1c2f5fef3c0780fada3a2713570cf738
Duncan Wilson <meddwilb@leeds.ac.uk>
2023-04-20 12:27:35 +0100

    Validation for BOSSS_problem objects

--- R/BOSSS_problem.R
+++ R/BOSSS_problem.R
@@ -29,15 +29,59 @@ new_BOSSS_problem <- function(sim_trial, design_space, hypotheses,
 # Validator
 validate_BOSSS_problem <- function(prob) {
 
-  # For example, check objective output numbers exist
-  objectives <- attr(prob, "objectives")
-  print(objectives$out_i)
+  # Do the functions have default arguments?
 
-  if(max(objectives$out_i) > attr(prob, "out_dimen")) {
-    stop(
-      "Objectives refer to non-existing simulation output exist",
-      call. = FALSE
-    )
+  # Does the simulation function return named outputs?
+  sim_out_names <- names(prob$simulation())
+  det_out_names <- NULL
+  if(!is.null(prob$det_func)){
+    # Does the deterministic function return named outputs?
+    det_out_names <- names(prob$det_func())
+  }
+  out_names <- c(sim_out_names, det_out_names)
+  if(!is.character(out_names)){
+    stop("Functions do not return named outputs")
+  } else {
+    # Do the named outputs appear in the constraints and objectives?
+    not_in <- !sapply(out_names, function(x) (x %in% prob$constraints$out) | (x %in% prob$objectives$out))
+    if(any(not_in)){
+      stop("One or more function outputs not not appear in any constraint or objective")
+    }
+  }
+
+  # Is the design space defined properly?
+  if(any( (prob$design_space$upper - prob$design_space$lower) <= 0 )){
+    stop("One or more design space ranges are <= 0")
+  }
+
+  # Are constraints defined properly?
+  if(any(prob$constraints$delta < 0 | prob$constraints$delta > 1)){
+    stop("Constraint deltas must lie in [0,1]")
+  }
+  if(!is.logical(prob$constraints$stoch)){
+    stop("Constraint stochasticity indicators must be logical")
+  }
+
+  # Are objectives defined properly?
+  if(!is.logical(prob$objectives$stoch)){
+    stop("Objective stochasticity indicators must be logical")
+  }
+
+  # Do all outputs referred to appear in the function outputs?
+  all_req_names <- c(prob$objectives$out, prob$constraints$out)
+  not_in <- !sapply(all_req_names, function(x) (x %in% out_names))
+  if(any(not_in)){
+    stop(paste("Outputs", all_req_names[not_in], " referred to in objectives
+               or constraints do not arise as named outputs from
+               the simulation or deterministic function"))
+  }
+
+  # Is there a deterministic function if required?
+  if(any( c(prob$constraints$stoch, prob$objectives$stoch) == F)) {
+    if(is.null(prob$det_func)) {
+      stop("Determinsitic function required by objectives and or constraints,
+           but none supplied")
+    }
   }
 
   prob
@@ -64,6 +108,6 @@ BOSSS_problem <- function(sim_trial, design_space, hypotheses,
 
   prob <- new_BOSSS_problem(sim_trial, design_space, hypotheses,
                                         constraints, objectives, det_func)
-  #validate_BOSSS_problem(prob)
+  validate_BOSSS_problem(prob)
   prob
 }
